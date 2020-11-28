407e0d995c7e715b946ee92e166d9b5b6030f2bd
lalit mendha <lalitmendha102@gmail.com>
2020-11-28 14:32:36 +0000

    init: initial commit

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var questionArray = [
-{
-  question: "",
-  answer: ""
-},
-{
-  question: "",
-  answer: ""
-}
-]
+console.log("Welcome to F.R.I.E.N.D.S quiz !!");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
